636f2f3983ff40dc769ced5f0a6081f900c3dd39
gospodinsmol <110425596+gospodinsmol@users.noreply.github.com>
2022-11-01 17:20:59 +0300

    Add bit operations tasks (#4)
    * Add bit opertions tasks
    
    * bugs fixed
    
    * bugs fixed

---
+++ Pack18/Task03/Program.cs
@@ -0,0 +1,6 @@
+﻿// Дан массив целых чисел, в котором все числа кроме одного встречаются четное количество раз,
+// одно число встречается нечетное количество раз. Найти это число. Решить задачу используя битовые операции.
+
+var nums = new[]{1, 2, 3, 4, 5, 6, 7, 7, 1, 2, 3, 4, 5, 6, 7};
+var result = nums.Aggregate((result, item) => result ^ item);
+Console.WriteLine($"The number {result} is repeated an odd number of times");
---
+++ Pack18/Task04/Program.cs
@@ -0,0 +1,7 @@
+﻿// Выяснить является ли число степенью двойки, используя битовые операции.
+
+Console.WriteLine("Enter number:");
+var input = int.Parse(Console.ReadLine());
+Console.WriteLine( (input & (input - 1)) == 0
+    ? $"Number {input} is a power of two"
+    : $"Число {input} isn`t a power of two");
